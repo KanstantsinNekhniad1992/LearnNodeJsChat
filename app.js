2fbceb5830736b6049d96437196303b20fd7237d
Kanstantsin Nekhniadovich <kanstantsin_nekhniadovich@epam.com>
2015-02-13 16:02:27 +0300

    screencasts 7-9:5:00

--- app.js
+++ app.js
@@ -7,12 +7,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var path = require('path');
-require('routes')(app);
+var HttpError = require('./error').HttpError;
+var router = express.Router();
+var session = require('express-session');
+var mongoStore = require('connect-mongo')(session);
+var mongoose = require('./libs/mongoose');
 
 var app = express();
 app.set('port', config.get('port'));
 app.set('views', __dirname+'/views');
 app.set('view engine', 'ejs');
+require('./routes')(app);
+app.use(require('./middleware/sendHttpError'));
+app.use(router);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,13 +29,57 @@ app.set('view engine', 'ejs');
 //    app.use(express.logger('default'));
 //}
 
+app.use(function(err, req, res, next) {
+    if(typeof err == 'number') {
+        err = new HttpError(err);
+    }
+
+    if(err instanceof HttpError) {
+        res.sendHttpError(err);
+    } else {
+        if(app.get('env') == 'development') {
+            console.log(err.message);
+        } else {
+            log.error(err);
+            err = new HttpError(500);
+            res.sendHttpError(err);
+        }
+    }
+});
+
 app.engine('ejs', require('ejs-locals'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+    secret: config.get('session:secret'),
+    key: config.get('session: key'),
+    cookie: config.get('session:cookie'),
+    resave: true,
+    saveUninitialized: false,
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection,
+        db: mongoose.connection.db,
+        session: session})
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
+
+app.use(function(req, res, next) {
+    var session = req.session;
+    if (session.views) {
+        session.views++;
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<p>views: ' + session.views + '</p>');
+        res.write('<p>expires in: ' + (session.cookie.maxAge / 1000) + 's</p>');
+        res.end();
+    } else {
+        session.views = 1;
+        res.end('welcome to the session demo. refresh!');
+    }
+});
 
 http.createServer(app).listen(app.get('port'), function() {
     log.info('Express is listening on port '+ config.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,12 @@
-var User = require('./models/user').User;
-var HttpError = require('error').HttpError;
-
 module.exports = function(app) {
 
-    app.get('/index', function(req, res, next) {
-        res.render('index', {
-            body: '<b>Hello</b>'
-        });
-    });
-    app.get('/users', function(req, res, next) {
-        User.find({}, function(err, user) {
-            if(err) throw err;
-            res.json(user);
-        });
-    });
+    app.get('/', require('./frontpage').get);
 
-    app.get('/user/:id', function(req, res, next) {
-        User.findById(req.params.id, function(err, user) {
-            res.json(user);
-        });
-    });
+    app.get('/login', require('./login').get);
+    app.post('/login', require('./login').post);
+
+    app.get('/chat', require('./chat').get);
+
 };
 
 
